cdd818aead69ed2d6f5d085f169e02514e62e64b
Levifer <b.pieteraerents@gmail.com>
2014-11-04 11:29:21 +0100

    Trying to configure maven for WSIMPORT

--- fleet-web/src/main/java/com/realdolmen/service/CarModelWebServiceClient.java
+++ fleet-web/src/main/java/com/realdolmen/service/CarModelWebServiceClient.java
@@ -1,13 +1,16 @@
 package com.realdolmen.service;
-        import com.realdolmen.domain.ws.carmodel.CarModels;
-        import com.realdolmen.domain.ws.carmodel.CarModelsResponse;
-        import com.realdolmen.domain.ws.carmodel.ObjectFactory;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Service;
-        import org.springframework.ws.client.core.WebServiceTemplate;
-        import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-        import org.springframework.ws.soap.client.core.SoapActionCallback;
-        import javax.xml.bind.JAXBElement;
+
+import com.realdolmen.domain.ws.carmodel.CarModels;
+import com.realdolmen.domain.ws.carmodel.CarModelsResponse;
+import com.realdolmen.domain.ws.carmodel.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+import javax.xml.bind.JAXBElement;
+
 /**
  * Created by AEIAT46 on 3-11-2014.
  */
@@ -16,7 +19,7 @@ public class CarModelWebServiceClient extends WebServiceGatewaySupport {
     @Autowired
     private WebServiceTemplate webServiceTemplate;
 
-    public CarModelsResponse getCarModelsByBrand(String brand){
+    public CarModelsResponse getCarModelsByBrand(String brand) {
         ObjectFactory of = new ObjectFactory();
         CarModels request = new CarModels();
         request.setBrand(brand);

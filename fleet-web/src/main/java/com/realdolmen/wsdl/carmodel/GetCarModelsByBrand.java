3853f5c81462fabc3371fb8c9c1835401855870a
Levifer <b.pieteraerents@gmail.com>
2014-11-05 11:46:18 +0100

    Merging

---
+++ fleet-web/src/main/java/com/realdolmen/wsdl/carmodel/GetCarModelsByBrand.java
@@ -0,0 +1,60 @@
+
+package com.realdolmen.wsdl.carmodel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getCarModelsByBrand complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getCarModelsByBrand">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="brand" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getCarModelsByBrand", propOrder = {
+    "brand"
+})
+public class GetCarModelsByBrand {
+
+    protected String brand;
+
+    /**
+     * Gets the value of the brand property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * Sets the value of the brand property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBrand(String value) {
+        this.brand = value;
+    }
+
+}
